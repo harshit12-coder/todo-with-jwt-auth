6ba3e1a2805ff3bc099251cfd6f5a0e07904fcb3
harshit12-coder <ht5627809@gmail.com>
2026-07-05 02:15:42 +0530

    fixed conflicts

--- main.py
+++ main.py
@@ -7,11 +7,8 @@ from routes.todo_routes import router as todo_router
 from core.limiter import limiter
 from slowapi import _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
-<<<<<<< HEAD
-app = FastAPI(title="Jarvis API - Ram Version")
-=======
-app = FastAPI(title="Jarvis API - Priya Version")
->>>>>>> branch-priya
+
+app = FastAPI(title="Jarvis API")
 
 app.state.limiter=limiter
 
